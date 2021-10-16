291123b7c60c0ac0f244c573b35877bdf000055c
Dominik Kozioł <dkoziol097@gmail.com>
2021-10-16 13:43:56 +0200

    creating book model and view

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,23 @@
+﻿using libApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace libApp.Controllers
+{
+    public class BooksController : Controller
+    {
+        public IActionResult Random()
+        {
+            var firstBook = new Book()
+            {
+                Name = "Law and punishment"
+            };
+
+
+            return View(firstBook);
+        }
+    }
+}
